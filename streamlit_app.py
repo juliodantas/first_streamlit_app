ae4b29beb86f3c45f7b9fc6fe8ca6da7a7272369
juliodantas <julio.dantas@yahoo.com.br>
2023-05-11 12:55:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Let's put a pick list here so they can pick the fruit they want to include
 
-st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #Let's put a pick list here so they can pick the fruit they want to include
 
