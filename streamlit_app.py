226a581ec4f3ad574217e9da478bca8404344452
juliodantas <julio.dantas@yahoo.com.br>
2023-05-11 12:40:25 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ st.text('🥗Omega 3 & Blueberry Oatmeal')
 st.text('🐔Kale, Spinach & Rocket Smoothie')
 st.text('🍞Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
