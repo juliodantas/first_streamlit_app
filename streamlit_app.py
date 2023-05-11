554bd2bdcdddfd497177c17486d1c1891863a476
juliodantas <julio.dantas@yahoo.com.br>
2023-05-11 12:52:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,10 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
+#Let's put a pick list here so they can pick the fruit they want to include
+
+st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
 #Display the table on the page
 
 st.dataframe(my_fruit_list)
