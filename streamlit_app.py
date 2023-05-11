8a6c961957f202a462322aa949f76e83c1f4b6aa
juliodantas <julio.dantas@yahoo.com.br>
2023-05-11 19:33:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import requests
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-st.title('My Parents New Healthy Diner')
+st.title('New Healthy Meals')
 
 st.header('🥣Breakfast Menu')
 st.text('🥗Omega 3 & Blueberry Oatmeal')
